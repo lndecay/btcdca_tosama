27a22912e91756e3835b1e7d87ba594a9333ae3a
lndecay <ezetomaselli95@gmail.com>
2025-02-22 15:02:41 -0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 semanal.py &  # Ejecuta el script semanal en segundo plano
-python3 comando.py    # Ejecuta el bot de comandos
+source /opt/venv/bin/activate  # Activa el entorno virtual en Railway
+python3 semanal.py &  # Ejecuta el bot semanal en segundo plano
+python3 comando.py  # Ejecuta el bot de comandos
