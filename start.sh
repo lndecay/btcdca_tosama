2d1df7284078b1b65ebd0fa14a14636fa562a659
lndecay <ezetomaselli95@gmail.com>
2025-02-22 16:24:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /opt/venv/bin/activate
 python3 semanal.py &   # Ejecuta el bot semanal en segundo plano
-python3 interes.py &   # Ejecuta el bot de interés compuesto en segundo plano
 python3 comando.py     # Ejecuta el bot principal (se mantiene en primer plano)
